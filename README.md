eee274ce42a3dda40252ecb1c6c6c06893205b74
ujrav <ujrav576@gmail.com>
2018-12-11 23:34:34 -0800

    initial push youtube-dl, can download a playlist

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+Dependencies
+youtube-dl
+	https://github.com/rg3/youtube-dl
+	pip install --upgrade youtube-dl
+
+ffmpeg
+	For Windows install:
+		https://stackoverflow.com/questions/30770155/ffprobe-or-avprobe-not-found-please-install-one
+		download ffmpeg for windows. Extract. Place files inside bin folder anywhere in the system path (e.g. if you're using anaconda: anaconda/scripts)
+
+
+
---
+++ test_playlist.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+    'outtmpl':'output/%(title)s.%(ext)s',
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/playlist?list=PL5p2SUre3pcF73gRIYfbQl3I4yl41UENE'])
